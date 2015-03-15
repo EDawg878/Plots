c63f30231bfbcf55357d9e5870105784d90acac0
EDawg878 <EDawg878@gmail.com>
2015-03-15 02:51:23 -0700

    updates

--- src/main/scala/com/edawg878/bungee/commands/package.scala
+++ src/main/scala/com/edawg878/bungee/commands/package.scala
@@ -1,7 +1,8 @@
-package com.edawg878.bukkit
+package com.edawg878.bungee
 
 import com.edawg878.common.MessageFormatter
-import org.bukkit.command.CommandSender
+import net.md_5.bungee.api.CommandSender
+import net.md_5.bungee.api.chat.TextComponent
 import scopt.ConsoleHandler.ConsoleHandler
 import scopt.CustomOptionParser
 
@@ -12,11 +13,13 @@ package object commands {
 
   val handler: ConsoleHandler[CommandSender] = new ConsoleHandler[CommandSender] {
 
-    override def print(sender: CommandSender, msg: String): Unit = sender.sendMessage(msg)
+    override def print(sender: CommandSender, msg: String): Unit =
+      sender.sendMessage(TextComponent.fromLegacyText(msg): _*)
 
     override def exit(): Unit = {}
 
-    override def printError(sender: CommandSender, msg: String): Unit = sender.sendMessage(MessageFormatter.ERROR + msg)
+    override def printError(sender: CommandSender, msg: String): Unit =
+      sender.sendMessage(TextComponent.fromLegacyText(MessageFormatter.ERROR + msg): _*)
   }
 
   class BukkitOptionParser[C](cmd: String) extends CustomOptionParser[C, CommandSender](cmd)(handler)
